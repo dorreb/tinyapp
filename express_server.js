aebaf68876f4fae4d1662811b550d61161df59eb
dorreb <ashley_ure@hotmail.com>
2023-01-23 13:43:36 -0500

    updating urls/:id templateVars object

--- express_server.js
+++ express_server.js
@@ -15,9 +15,10 @@ app.get('/urls', (req, res) => {
 });
 
 app.get('/urls/:id', (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase['b2xVn2'] };
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render('urls_show', templateVars);
 });
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
